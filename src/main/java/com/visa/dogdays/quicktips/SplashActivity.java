9aa0c73913a02885a735d2fc5787fa8d8566e4a2
Drew Richardson <drewrichardson1211@gmail.com>
2015-12-12 15:54:09 -0700

    add typeface

--- src/main/java/com/visa/dogdays/quicktips/SplashActivity.java
+++ src/main/java/com/visa/dogdays/quicktips/SplashActivity.java
@@ -2,12 +2,14 @@ package com.visa.dogdays.quicktips;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -20,6 +22,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+//Set typeface to Myriad
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "MYRIADPRO-REGULAR.ttf");
+        TextView myTextview = (TextView)findViewById(R.id.textViewSplash);
+        myTextview.setTypeface(myTypeface);
     }
+
+
+
 
 }
