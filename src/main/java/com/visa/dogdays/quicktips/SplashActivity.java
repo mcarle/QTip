0604f958a17d35e9ddbc37ab5141a22c94938642
Matthew Carle <mcarle@Matthews-MacBook-Pro.local>
2015-12-12 15:54:47 -0700

    added splash screen transition

--- src/main/java/com/visa/dogdays/quicktips/SplashActivity.java
+++ src/main/java/com/visa/dogdays/quicktips/SplashActivity.java
@@ -15,11 +15,24 @@ import android.view.View;
  */
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000; // show splash for 3 seconds.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // set delayed transition to menu screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // show splash screen on a timer, then transition to main screen
+                Intent i = new Intent(SplashActivity.this, MenuActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            };
+        }, SPLASH_TIME_OUT);
     }
 
 }
