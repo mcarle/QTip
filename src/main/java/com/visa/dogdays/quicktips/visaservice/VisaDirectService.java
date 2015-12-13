5f9426ed5ff9144cce86657cfbaee68a6c8d0110
Matthew Carle <mcarle@Matthews-MacBook-Pro.local>
2015-12-12 17:52:04 -0700

    added persistent field to manifest

--- src/main/java/com/visa/dogdays/quicktips/visaservice/VisaDirectService.java
+++ src/main/java/com/visa/dogdays/quicktips/visaservice/VisaDirectService.java
@@ -5,5 +5,24 @@ package com.visa.dogdays.quicktips.visaservice;
  */
 public class VisaDirectService {
 
+    private String userId;
+    private String  pwd;
+    private String keyStorePath;
+    private String keyStorePwd;
+    private String privateKeyPwd;
+
+    public VisaDirectService(String userId, String pwd, String keyStorePath,
+                             String keyStorePwd, String privateKeyPwd){
+        this.userId = userId;
+        this.pwd = pwd;
+        this.keyStorePath = keyStorePath;
+        this.keyStorePwd = keyStorePwd;
+        this.privateKeyPwd = privateKeyPwd;
+
+    }
+
+    public void sendFundsTransferRequest(){
+        //OkHttpClient client = new OkHttpClient();
+    }
 
 }
