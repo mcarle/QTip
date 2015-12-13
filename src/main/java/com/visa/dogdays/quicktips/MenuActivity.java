a57a7070f52b09735a1bb7b1d37d239fe551d1de
Drew Richardson <drewrichardson1211@gmail.com>
2015-12-12 19:17:07 -0700

    QR info page

--- src/main/java/com/visa/dogdays/quicktips/MenuActivity.java
+++ src/main/java/com/visa/dogdays/quicktips/MenuActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-
+import android.view.View;
 
 
 public class MenuActivity extends Activity {
@@ -15,6 +15,18 @@ public class MenuActivity extends Activity {
         setContentView(R.layout.activity_menu);
         ReplaceFont.replaceDefaultFont(this, "DEFAULT", "MYRIADPRO-REGULAR.ttf");
 
+    }
+
+    public void sendMessageMoney(View view)
+    {
+        Intent i = new Intent(MenuActivity.this, SendFundsActivity.class);
+        startActivity(i);
+    }
+
+    public void sendMessageCode(View view)
+    {
+        Intent i = new Intent(MenuActivity.this, QRInfoActivity.class);
+        startActivity(i);
     }
 
 }
